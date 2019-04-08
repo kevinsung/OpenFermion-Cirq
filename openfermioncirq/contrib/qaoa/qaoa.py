e6bf61c1622f555bee82a7d4f5529b1b64466a51
Kevin J. Sung <kevinjefferysung@gmail.com>
2019-04-08 11:34:51 -0700

    test

--- openfermioncirq/contrib/qaoa/qaoa.py
+++ openfermioncirq/contrib/qaoa/qaoa.py
@@ -1,3 +1,4 @@
+import numpy
 import cirq
 
 from openfermioncirq.variational import VariationalAnsatz
@@ -9,18 +10,19 @@ class QAOAMaxCutAnsatz(VariationalAnsatz):
                  graph,
                  p,
                  adiabatic_evolution_time=None,
-                 qubits
+                 qubits=None
                  ):
         self.graph = graph
         self.p = p
 
         if adiabatic_evolution_time is None:
             adiabatic_evolution_time = float(graph.size())
+        self.adiabatic_evolution_time = adiabatic_evolution_time
 
         super().__init__(qubits)
 
     def params(self):
-        for i in range(p):
+        for i in range(self.p):
             yield LetterWithSubscripts('gamma', i)
             yield LetterWithSubscripts('beta', i)
 
@@ -43,3 +45,26 @@ class QAOAMaxCutAnsatz(VariationalAnsatz):
             yield cirq.XPowGate(exponent=beta).on_each(*qubits)
 
     def default_initial_params(self):
+        total_time = self.adiabatic_evolution_time
+        step_time = total_time / self.p
+
+        params = []
+        for i in range(self.p):
+            interpolation_progress = 0.5 * (2 * i + 1) / self.p
+            params.append(_canonicalize_exponent(
+                -interpolation_progress * step_time / numpy.pi, 2))
+            params.append(_canonicalize_exponent(
+                (1-interpolation_progress) * step_time / numpy.pi, 2))
+
+        return numpy.array(params)
+
+
+def _canonicalize_exponent(exponent: float, period: int) -> float:
+    # Shift into [-p/2, +p/2).
+    exponent += period / 2
+    exponent %= period
+    exponent -= period / 2
+    # Prefer (-p/2, +p/2] over [-p/2, +p/2).
+    if exponent <= -period / 2:
+        exponent += period  # coverage: ignore
+    return exponent
